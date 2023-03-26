a002af38a872947d955176d12a548e15b3ab336d
octubree <jorgitobaliero@gmail.com>
2023-03-26 12:45:39 -0300

    probando cosas

---
+++ java2.js
@@ -0,0 +1,91 @@
+/* Please ❤ this if you like it! */
+
+(function($) { "use strict";
+
+	// Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px";
+		t.style.top = n.clientY + "px";
+		e.style.left = n.clientX + "px";
+		e.style.top = n.clientY + "px";
+		i.style.left = n.clientX + "px";
+		i.style.top = n.clientY + "px";
+    });
+
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+
+    function n() {
+        e.classList.add("hover");
+        i.classList.add("hover");
+    }
+
+    function s() {
+        e.classList.remove("hover");
+        i.classList.remove("hover");
+    }
+
+    s();
+
+    for (var a = document.querySelectorAll(".hover-target"), r = a.length - 1; r >= 0; r--) {
+        o(a[r]);
+    }
+
+    function o(t) {
+        t.addEventListener("mouseover", n);
+        t.addEventListener("mouseout", s);
+    }
+
+	// About page
+
+	$(".about-text").on('click', function() {
+		$("body").addClass("about-on");
+	});
+
+	$(".about-close").on('click', function() {
+		$("body").removeClass("about-on");
+	});
+
+	// Contact page
+
+	$(".contact-text").on('click', function() {
+		$("body").addClass("contact-on");
+	});
+
+	$(".contact-close").on('click', function() {
+		$("body").removeClass("contact-on");
+	});
+
+	// Travel portfolio page
+
+	$(".travel").on('click', function() {
+		$("body").addClass("travel-on");
+	});
+
+	$(".travel-close").on('click', function() {
+		$("body").removeClass("travel-on");
+	});
+
+	// Wildlife portfolio page
+
+	$(".wildlife").on('click', function() {
+		$("body").addClass("wildlife-on");
+	});
+
+	$(".wildlife-close").on('click', function() {
+		$("body").removeClass("wildlife-on");
+	});
+
+	// Nature portfolio page
+
+	$(".nature").on('click', function() {
+		$("body").addClass("nature-on");
+	});
+
+	$(".nature-close").on('click', function() {
+		$("body").removeClass("nature-on");
+	});
+
+})(jQuery);
